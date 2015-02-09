1fd4897a983c3abf1a5e1f62479068d79de68ad7
jeroenvere <jeroen.verellen@basware.com>
2015-02-09 19:08:52 +0100

    make tests work

--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
-var should = require('chai').should(),
+var expect = require('chai').expect,
     namedparam = require('../index'),
     verify = namedparam.verify;
 
 describe('#verify', function() {
   it('uses defaults', function() {
-    verify({'key1':'val1', 'key2':'val2'},{}).should.equal({'key1':'val1', 'key2':'val2'});
+    var res = verify({'key1':'val1', 'key2':'val2'},{});
+    expect(res).to.have.property('key1').to.equal('val1');
+    expect(res).to.have.property('key2').to.equal('val2');
   });
 
   it('use value if provided', function() {
-    verify({'key1':'val1', 'key2':'default'},{'key2':'val2'}).should.equal({'key1':'val1', 'key2':'val2'});
+    var res = verify({'key1':'val1', 'key2':'default'},{'key2':'val2'});
+    expect(res).to.have.property('key1').to.equal('val1');
+    expect(res).to.have.property('key2').to.equal('val2');
   });
 
 });
