9bbcf0ed10d7712e647d30cb0dfd1e43c5529a1c
jeroenvere <jeroen.verellen@basware.com>
2015-02-10 22:21:36 +0100

    refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var _ = require('underscore');
 
 module.exports = {
-    mandatory : Object.freeze({
+    mandatory: Object.freeze({
         STRING: 0,
         NUMBER: 1,
         BOOLEAN: 2,
@@ -12,22 +12,26 @@ module.exports = {
         var verifiedParamObj = {};
 
         function checkMandatory(key) {
-        	//should be this.mandatory.STRING  iso 0
+            //should be this.mandatory.STRING  iso 0
             if (parameterObjectContract[key] === 0 && !_.isString(parameterObject[key]))
                 throw "Mandatory parameter missing";
         };
 
-        function checkCorrectType(key){
-        	if (typeof parameterObjectContract[key] !== typeof verifiedParamObj[key])
+        function checkCorrectType(key) {
+            if (typeof parameterObjectContract[key] !== typeof verifiedParamObj[key])
                 throw typeof parameterObjectContract[key] + " type expected";
         };
 
+        function useValueOrDefaultFor(key, value) {
+            return (_.isUndefined(parameterObject[key]) || _.isNull(parameterObject[key])) ? value : parameterObject[key];
+        }
+
         _.each(parameterObjectContract, function(value, key, list) {
             checkMandatory(key);
-            verifiedParamObj[key] = (_.isUndefined(parameterObject[key]) || _.isNull(parameterObject[key])) ? value : parameterObject[key];
+            verifiedParamObj[key] = useValueOrDefaultFor(key, value);
             checkCorrectType(key);
         });
-        
+
         return verifiedParamObj;
     }
 };
