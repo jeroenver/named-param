f7f478c0cfff3261a0980e5dc0fe2fabd1dfbf3d
jeroenvere <jeroen.verellen@basware.com>
2015-02-10 22:18:30 +0100

    add type checking + mandatory check for string

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+"use strict";
 var _ = require('underscore');
 
 module.exports = {
-	verify : function(parameterObjectContract, parameterObject){
-		var verifiedParamObj = {};
-		_.each(parameterObjectContract, function(value, key, list){
-			verifiedParamObj[key] = (_.isUndefined(parameterObject[key]) || _.isNull(parameterObject[key])) ? value : parameterObject[key];
-		});
-		return verifiedParamObj;
-	}
+    mandatory : Object.freeze({
+        STRING: 0,
+        NUMBER: 1,
+        BOOLEAN: 2,
+        FUNCTION: 3
+    }),
+    verify: function(parameterObjectContract, parameterObject) {
+        var verifiedParamObj = {};
+
+        function checkMandatory(key) {
+        	//should be this.mandatory.STRING  iso 0
+            if (parameterObjectContract[key] === 0 && !_.isString(parameterObject[key]))
+                throw "Mandatory parameter missing";
+        };
+
+        function checkCorrectType(key){
+        	if (typeof parameterObjectContract[key] !== typeof verifiedParamObj[key])
+                throw typeof parameterObjectContract[key] + " type expected";
+        };
+
+        _.each(parameterObjectContract, function(value, key, list) {
+            checkMandatory(key);
+            verifiedParamObj[key] = (_.isUndefined(parameterObject[key]) || _.isNull(parameterObject[key])) ? value : parameterObject[key];
+            checkCorrectType(key);
+        });
+        
+        return verifiedParamObj;
+    }
 };
--- test/test.js
+++ test/test.js
@@ -1,18 +1,42 @@
 var expect = require('chai').expect,
     namedparam = require('../index'),
-    verify = namedparam.verify;
+    verify = namedparam.verify,
+    mandatory = namedparam.mandatory;
 
-describe('#verify', function() {
-  it('uses defaults', function() {
-    var res = verify({'key1':'val1', 'key2':'val2'},{});
-    expect(res).to.have.property('key1').to.equal('val1');
-    expect(res).to.have.property('key2').to.equal('val2');
-  });
+describe('verify optional parameters', function() {
+    it('uses defaults', function() {
+        var res = verify({
+            'key1': 'val1',
+            'key2': 'val2'
+        }, {});
+        expect(res).to.have.property('key1').to.equal('val1');
+        expect(res).to.have.property('key2').to.equal('val2');
+    });
 
-  it('use value if provided', function() {
-    var res = verify({'key1':'val1', 'key2':'default'},{'key2':'val2'});
-    expect(res).to.have.property('key1').to.equal('val1');
-    expect(res).to.have.property('key2').to.equal('val2');
-  });
+    it('use value if provided', function() {
+        var res = verify({
+            'key1': 'val1',
+            'key2': 'default'
+        }, {
+            'key2': 'val2'
+        });
+        expect(res).to.have.property('key1').to.equal('val1');
+        expect(res).to.have.property('key2').to.equal('val2');
+    });
 
+    it('wrong type throws exception: boolean', function() {
+        expect(verify.bind(null, {
+            'key1': true
+        }, {
+            'key1': 'true'
+        })).to.throw('boolean type expected');
+    });
+});
+
+describe('verify mandatory parameters', function() {
+    it('missing parameter throws exception', function() {
+        expect(verify.bind(null, {
+            'key1': mandatory.STRING
+        }, {})).to.throw('Mandatory parameter missing');
+    });
 });
