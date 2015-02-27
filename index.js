a62b1a84a682715831349bbb7083d2cd93b048e9
jeroenvere <jeroen.verellen@basware.com>
2015-02-27 21:21:33 +0100

    adding tests

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 "use strict";
 var _ = require('underscore');
+var mandatoryEnum = Object.freeze({
+    //use objects for better equals checking
+    STRING: {
+        'eq': 'string'
+    },
+    NUMBER: 1,
+    BOOLEAN: 2,
+    FUNCTION: 3
+});
 
 module.exports = {
-    mandatory: Object.freeze({
-        STRING: 0,
-        NUMBER: 1,
-        BOOLEAN: 2,
-        FUNCTION: 3
-    }),
+    mandatory: mandatoryEnum,
     verify: function(parameterObjectContract, parameterObject) {
         var verifiedParamObj = {};
 
         function checkMandatory(key) {
-            //should be this.mandatory.STRING  iso 0
-            if (parameterObjectContract[key] === 0 && !_.isString(parameterObject[key]))
+            if (parameterObjectContract[key] === mandatoryEnum.STRING && !_.isString(parameterObject[key])) {
                 throw "Mandatory parameter missing";
+            }
         };
 
         function checkCorrectType(key) {
-            if (typeof parameterObjectContract[key] !== typeof verifiedParamObj[key])
+            if (typeof parameterObjectContract[key] !== typeof verifiedParamObj[key]) {
                 throw typeof parameterObjectContract[key] + " type expected";
+            }
         };
 
         function useValueOrDefaultFor(key, value) {
--- test/test.js
+++ test/test.js
@@ -5,38 +5,101 @@ var expect = require('chai').expect,
 
 describe('verify optional parameters', function() {
     it('uses defaults', function() {
-        var res = verify({
+        var contract = {
             'key1': 'val1',
             'key2': 'val2'
-        }, {});
+        };
+        var parameter = {};
+        var res = verify(contract, parameter);
         expect(res).to.have.property('key1').to.equal('val1');
         expect(res).to.have.property('key2').to.equal('val2');
     });
 
-    it('use value if provided', function() {
-        var res = verify({
+    it('use value if provided: string', function() {
+        var contract = {
             'key1': 'val1',
             'key2': 'default'
-        }, {
+        };
+        var parameter = {
             'key2': 'val2'
-        });
+        };
+        var res = verify(contract, parameter);
         expect(res).to.have.property('key1').to.equal('val1');
         expect(res).to.have.property('key2').to.equal('val2');
+    });
+
+    it('use value if provided: numeric', function() {
+        var contract = {
+            'key1': 'val1',
+            'key2': 0
+        };
+        var parameter = {
+            'key2': 1
+        };
+        var res = verify(contract, parameter);
+        expect(res).to.have.property('key1').to.equal('val1');
+        expect(res).to.have.property('key2').to.equal(1);
+    });
+
+    it('use value if provided: boolean', function() {
+        var contract = {
+            'key1': 'val1',
+            'key2': true
+        };
+        var parameter = {
+            'key2': false
+        };
+        var res = verify(contract, parameter);
+        expect(res).to.have.property('key1').to.equal('val1');
+        expect(res).to.have.property('key2').to.equal(false);
+    });
+
+    it('use value if provided: function', function() {
+        var contract = {
+            'key1': 'val1',
+            'key2': function(){return "default";}
+        };
+        var parameter = {
+            'key2': function(){return "impl";}
+        };
+        var res = verify(contract, parameter);
+        expect(res).to.have.property('key1').to.equal('val1');
+        expect(res).to.have.property('key2').to.be.a('function');
+        expect(res.key2()).to.equal('impl');
     });
 
     it('wrong type throws exception: boolean', function() {
-        expect(verify.bind(null, {
+        var contract = {
             'key1': true
-        }, {
+        };
+        var parameter = {
             'key1': 'true'
-        })).to.throw('boolean type expected');
+        };
+        expect(verify.bind(null, contract, parameter)).to.throw('boolean type expected');
     });
 });
 
 describe('verify mandatory parameters', function() {
     it('missing parameter throws exception', function() {
-        expect(verify.bind(null, {
+        var contract = {
             'key1': mandatory.STRING
-        }, {})).to.throw('Mandatory parameter missing');
+        };
+        var parameter = {};
+        expect(verify.bind(null, contract, parameter)).to.throw('Mandatory parameter missing');
+    });
+
+    it('mandatoryEnum should be unique values', function() {
+        var contract = {
+            'key1': 0
+        };
+        var parameter = {
+            'key1': 1
+        };
+        var res = verify(contract, parameter);
+        expect(res).to.have.property('key1').to.equal(1);
+    });
+
+    it('mandatory string value, throw if not string', function() {
+        fail('not impl');
     });
 });
